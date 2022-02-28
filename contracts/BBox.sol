642cf72b8c0e55fb2cfef47d41bb1199b37b7432
Demi-LL <liuliunft@gmail.com>
2022-02-28 16:04:35 +0800

    feat: 實作基本版 ERC721.

--- contracts/BBox.sol
+++ contracts/BBox.sol
@@ -1,15 +1,191 @@
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 
-contract BBox {
-    string show;
+// token id 從 0 開始
+contract BBox is IERC721, IERC721Metadata {
+    using Address for address;
 
-    constructor () {
-        show = "DM";
+    uint16 constant public MAX_SUPPLY = 9999;
+
+    uint256 public totalSupply;
+
+    string override public name;
+
+    string override public symbol;
+
+    mapping(uint256 => address) private _owners;
+
+    mapping(address => uint256) private _balances;
+
+    // 特定 token 的第三方授權
+    mapping(uint256 => address) private _tokenApprovals;
+
+    // 特定帳戶的第三方授權
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
+
+    /**
+    * name: BBOX
+    * symbol: BOX
+    */
+    constructor (string memory _name, string memory _symbol) {
+        name = _name;
+
+        symbol = _symbol;
+
+        totalSupply = 0;
     }
 
-    function hello() public view returns (string memory){
-        return show;
+    modifier nonCaller(address account) {
+        require(account != msg.sender, "Cannot use your own address.");
+        _;
+    }
+
+    modifier nonZeroAddress(address account) {
+        require(account != address(0), "Cannot use zero address.");
+        _;
+    }
+
+    modifier existToken(uint256 tokenId) {
+        require(totalSupply > tokenId);
+        _;
+    }
+
+    modifier onlyApprovedOrOwner(address spender, uint256 tokenId) {
+        address owner = this.ownerOf(tokenId);
+        require(spender == owner || getApproved(tokenId) == spender || isApprovedForAll(owner, spender), "You don't have permission to manipulate it.");
+        _;
+    }
+
+    modifier checkSupportERC721(address from, address to, uint256 tokenId, bytes memory _data) {
+        _;
+        require(
+            _checkOnERC721Received(from, to, tokenId, _data),
+            "ERC721: transfer to non ERC721Receiver implementer."
+        );
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(IERC165) returns (bool) {
+        return
+            interfaceId == type(IERC721).interfaceId ||
+            interfaceId == type(IERC721Metadata).interfaceId;
+    }
+
+    // TODO
+    function tokenURI(uint256 tokenId) override external view returns (string memory) {}
+    
+    function balanceOf(address owner) override external view nonZeroAddress(owner) returns (uint256 balance) {
+        return _balances[owner];
+    }
+
+    /**
+    * tokenId: 0 ~ MAX_SUPPLY
+    */
+    function ownerOf(uint256 tokenId) override external view existToken(tokenId) returns (address owner) {
+        return _owners[tokenId];
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId) override external {
+        this.safeTransferFrom(from, to, tokenId, "");
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes calldata data
+    ) override external checkSupportERC721(from, to, tokenId, data) {
+        _transfer(from, to, tokenId);
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) override external {
+        _transfer(from, to, tokenId);
+    }
+
+    /**
+     * 設定第三方授權
+     */
+    function approve(address to, uint256 tokenId) override external
+        existToken(tokenId) nonZeroAddress(to) nonCaller(to) onlyApprovedOrOwner(to, tokenId)
+    {
+        _tokenApprovals[tokenId] = to;
+
+        emit Approval(msg.sender, to, tokenId);
+    }
+
+    /**
+     * 取得被授權的帳戶
+     */
+    function getApproved(uint256 tokenId) override public view existToken(tokenId) returns (address operator) {
+        return _tokenApprovals[tokenId];
+    }
+
+    function setApprovalForAll(address operator, bool _approved) override external nonZeroAddress(operator) nonCaller(operator) {
+        _operatorApprovals[msg.sender][operator] = _approved;
+
+        emit ApprovalForAll(msg.sender, operator, _approved);
+    }
+
+    function isApprovedForAll(address owner, address operator) override public view returns (bool) {
+        return _operatorApprovals[owner][operator];
+    }
+
+    function _transfer(address from, address to, uint256 tokenId) private
+        existToken(tokenId) nonZeroAddress(to) onlyApprovedOrOwner(from, tokenId)
+    {
+        // 取消先前對該 token 的所有第三方授權
+        this.approve(address(0), tokenId);
+
+        _owners[tokenId] = to;
+        _balances[from]--;
+        _balances[to]++;
+
+        emit Transfer(from, to, tokenId);
+    }
+
+    function _mint(address to, uint256 tokenId) private nonZeroAddress(to) {
+        require(tokenId >= totalSupply, "The token already been minted.");
+
+        _balances[to]++;
+        _owners[tokenId] = to;
+
+        emit Transfer(address(0), to, tokenId);
+    }
+
+    function _safeMint(address to, uint256 tokenId, bytes memory _data) internal virtual
+        checkSupportERC721(address(0), to, tokenId, _data)
+    {
+        _mint(to, tokenId);
+    }
+
+    /**
+    * 若轉移的目標帳戶 to 為合約帳戶，檢查是否支援 ERC721
+    */
+    function _checkOnERC721Received(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) private returns (bool) {
+        if (to.isContract()) {
+            try IERC721Receiver(to).onERC721Received(msg.sender, from, tokenId, _data) returns (bytes4 retval) {
+                return retval == IERC721Receiver.onERC721Received.selector;
+            } catch (bytes memory reason) {
+                if (reason.length == 0) {
+                    revert("ERC721: transfer to non ERC721Receiver implementer");
+                } else {
+                    assembly {
+                        revert(add(32, reason), mload(reason))
+                    }
+                }
+            }
+        } else {
+            return true;
+        }
     }
 }
