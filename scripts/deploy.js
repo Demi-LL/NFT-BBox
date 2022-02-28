9a9dba07e3d0334a12423839b10b2fe125d5f0da
Demi-LL <liuliunft@gmail.com>
2022-02-28 18:32:48 +0800

    test: 加入簡易的測試
    1. 測試初始化合約後的 metadata 回傳
    2. 測試 owner 身份呼叫 mintNFT(), airdrop() 行為的正確性

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+    const BBox = await ethers.getContractFactory("BBox");
+    const contract = await BBox.deploy("BBOX", "BOX");
+
+    console.log("BBOX deployed to:", contract.address);
+}
+
+main()
+.then(() => process.exit(0))
+.catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
--- test/BBox.js
+++ test/BBox.js
@@ -1,12 +1,67 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("BBox", function () {
-  it("Should return DM", async function () {
-    const BBox = await ethers.getContractFactory("BBox");
-    const contract = await BBox.deploy();
-    await contract.deployed();
+async function deployedContract() {
+  const BBox = await ethers.getContractFactory("BBox");
+  const contract = await BBox.deploy("BBOX", "BOX");
+  await contract.deployed();
 
-    expect(await contract.hello()).to.equal("DM");
+  return contract;
+}
+
+let contract;
+describe("BBox", function () {
+  beforeEach(async function() {
+    contract = await deployedContract();
+  });
+
+  describe("Metadata", function () {
+    it("Should return 0", async function () {
+      expect(await contract.totalSupply()).to.equal(0);
+    });
+
+    it("Should return BBOX", async function () {
+      expect(await contract.name()).to.equal("BBOX");
+    });
+
+    it("Should return BOX", async function () {
+      expect(await contract.symbol()).to.equal("BOX");
+    });
+  });
+
+  describe("Owner", function () {
+    let owner;
+    let account1;
+
+    beforeEach(async function() {
+      [owner, account1] = await ethers.getSigners();
+      await contract.connect(owner);
+    });
+
+    /**
+    * TODO: find methods to catch require, revert error when call contract
+    */
+    describe("Not action yet", function () {
+      it ("Should return 0", async function () {
+        expect(await contract.balanceOf(owner.address)).to.equal(0);
+      });
+
+      it("Should return 1 after mintNFT()", async function () {
+        await contract.setOpeningMax(1);
+        await contract.setPurchaseStatus(true);
+        await contract.mintNFT();
+
+        expect(await contract.balanceOf(owner.address)).to.equal(1);
+      });
+
+      it("Should return 1 after airdrop()", async function () {
+        await contract.setOpeningMax(1);
+        await contract.setPurchaseStatus(true);
+        await contract.airdrop(account1.address, 1);
+
+        expect(await contract.balanceOf(owner.address)).to.equal(0);
+        expect(await contract.balanceOf(account1.address)).to.equal(1);
+      });
+    });
   });
 });
